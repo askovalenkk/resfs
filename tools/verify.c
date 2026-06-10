41de85671d49b4b6c5e9100538b81d704b016d6a
Andrei Kovalenko <a.kovalenko1079@gmail.com>
2026-06-11 02:44:34 +0300

    Rename resfs-verify.c to verify.c

--- tools/verify.c
+++ tools/verify.c
@@ -1,5 +1,5 @@
 /*
-*  resfs/tools/resfs-verify/resfs-verify.c
+*  resfs/tools/resfs-verify/verify.c
 *  License – PetalSoft General Non-commercial Software License (PGSL-NC)
 *  Copyright (c) 2026 Andrei Kovalenko
 */
