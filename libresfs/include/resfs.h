745952686e87549e7d426061506dcc7e8fba514d
Andrei Kovalenko <a.kovalenko1079@gmail.com>
2026-06-10 03:45:52 +0300

    Define RESFS signatures in resfs.h
    Added various signature definitions for RESFS.

--- libresfs/include/resfs.h
+++ libresfs/include/resfs.h
@@ -1,1 +1,20 @@
+/*
+*  resfs/libresfs/include/resfs.h
+*  License – PetalSoft General Non-commercial Software License (PGSL-NC)
+*  Copyright (c) 2026 Andrei Kovalenko
+*/
 
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+
+#define BH_SIG "RESFS PARTITION "
+#define ROOT_SIG "RESFS ROOT "
+#define EOP_TAIL_TEXT "END OF RESFS PARTITION"
+#define EOP_SIG "ResFSEOP"
+#define SMI_SIG "ResFSSMI"
+#define DLI_SIG "ResFSDLI"
+#define SNAP_SIG "ResFSSNP"
+#define SEG_SIG "ResFSSEG"
+#define SEG_END_SIG "ResFSEND"
