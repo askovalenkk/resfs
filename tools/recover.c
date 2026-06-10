08a40e7e4e981ae5c6ed81f53c40e234642c57c4
Andrei Kovalenko <a.kovalenko1079@gmail.com>
2026-06-11 02:44:50 +0300

    Update and rename tools/resfs-recover/resfs-recover.c to tools/recover.c

--- tools/recover.c
+++ tools/recover.c
@@ -1,5 +1,5 @@
 /*
-*  resfs/tools/resfs-recover/resfs-recover.c
+*  resfs/tools/recover.c
 *  License – PetalSoft General Non-commercial Software License (PGSL-NC)
 *  Copyright (c) 2026 Andrei Kovalenko
 */
